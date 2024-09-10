4d853a5ecd12c85d8c3dd330ebb80665bcc1fe30
Daniel Oliveira <daniel.oliveira@innux.com>
2024-09-10 16:27:50 +0100

    Builds the responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
 import './App.css';
-import { Button } from '@chakra-ui/react';
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Grid,
+  GridItem,
+  Show,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 function App() {
-  return <Button colorScheme="blue"></Button>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        md: `"nav nav" "aside main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem bg="pink.300" area="nav">
+        Nav
+      </GridItem>
+      <Show above="md">
+        <GridItem bg="green.300" area="aside">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="blue.300" area="main">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
