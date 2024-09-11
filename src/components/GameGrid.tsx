719e5f796a5a56e0c00f7f4b88a07ae2c97ed04f
Daniel Oliveira <daniel.oliveira@innux.com>
2024-09-11 15:39:39 +0100

    Refactores the game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,25 +13,23 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { error, data: games, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-
-      <SimpleGrid columns={{ sm: 1, md: 3, lg: 4, xl: 4 }} pb={3} spacing={6}>
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-
-        {games.map((g) => (
-          <GameCardContainer key={g.id}>
-            <GameCard game={g} />
+    <SimpleGrid columns={{ sm: 1, md: 3, lg: 4, xl: 4 }} pb={3} spacing={6}>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+
+      {games.map((g) => (
+        <GameCardContainer key={g.id}>
+          <GameCard game={g} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
