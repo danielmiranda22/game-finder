0da09662c113104c514b9ceff058a0df99d98e1a
Daniel Oliveira <daniel.oliveira@innux.com>
2024-09-14 22:03:57 +0100

    Fix the NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,22 @@ import { HStack, Image, Text, useColorMode } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   const { colorMode } = useColorMode();
 
   return (
     <HStack p={2} bg={colorMode === 'light' ? 'gray.100' : ''}>
-      <Image rounded="full" boxSize="60px" src={logo} boxShadow="lg" />
+      <Link to="/">
+        <Image
+          rounded="full"
+          boxSize="60px"
+          src={logo}
+          boxShadow="lg"
+          objectFit="cover"
+        />
+      </Link>
       <SearchInput />
       <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,19 @@ import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { BsBinoculars } from 'react-icons/bs';
 import useGamesStore from '../store';
+import { useNavigate } from 'react-router-dom';
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGamesStore((s) => s.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         if (ref.current) setSearchText(ref.current?.value);
+        navigate('/');
       }}
     >
       <InputGroup>
